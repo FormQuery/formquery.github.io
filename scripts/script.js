901ef830fe9186f25111a5e43735ff2ae0969e19
Wyatt Hardin <whardin1010@gmail.com>
2025-03-01 14:01:32 -0500

    Added really bad auth

--- scripts/script.js
+++ scripts/script.js
@@ -1,6 +1,8 @@
 const menuToggle = document.getElementById('menu-toggle');
 const menu = document.getElementById('menu');
 const overlay = document.getElementById('overlay');
+const secrets = document.querySelectorAll('#secret');
+const secretLink = document.getElementById('secretLink');
 
 menuToggle.addEventListener('click', () => {
     const isOpen = menu.classList.contains('hidden');
@@ -14,3 +16,35 @@ overlay.addEventListener('click', () => {
     overlay.classList.add('hidden');
     menuToggle.textContent = '☰';
 });
+
+
+const DUMMY_SECRET = 'supersecret';
+const adminView = () => {
+    const storage = localStorage.getItem('isAdmin');
+    if (storage && Boolean(storage)) return true;
+
+    const secret = new URLSearchParams(window.location.search).get('secret');
+    
+    if (!secret || secret !== DUMMY_SECRET) {
+        secretLink.classList.add('hidden');
+
+        if (location.pathname === '/scripts.html')
+            navigateHome();
+        return false;
+    }
+
+    localStorage.setItem('isAdmin', true);
+    return true;
+}
+
+const navigateHome = () => {
+    location.href = '/index.html';
+}
+
+const onLoad = () => {
+    if (!adminView()) {
+        secrets.forEach(el => el.remove());
+    }
+}
+
+onLoad();
